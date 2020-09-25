5e47f0ae21747ee165f256b56cb99a4ea7ff7b48
Rickard-Kujala <rickard.kujala89@gmail.com>
2020-09-25 14:26:47 +0200

    Initial commit

---
+++ Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TjuvOchPolis
+{
+    class Person
+    {
+        public int CurentPositionX { get; set; }
+        public int CurentPositionY { get; set; }
+        public int Direction { get; set; }
+
+        public static List<Person> PersonList = new List<Person>();
+        public Person(int curentpositionX, int curentpositionY, int direction)
+        {
+            CurentPositionX = curentpositionX;
+            CurentPositionY = curentpositionY;
+            Direction = direction;
+        }
+        public static void FillCityWithPeople()
+        {
+            Citizen.CreateCitizens(3);
+            
+        }
+        public static void Move(List<Person> Personlist, int i, int j)//Stoppa in lista med persiner och fp dom att röra sig
+        {
+            foreach (var Person in PersonList)
+            {
+                if (Person.Direction == 1)//Om Direction är=1 går personen åt höger
+                {
+                    if (Person.CurentPositionY==i)//Går åt höger
+                    {
+                        Person.CurentPositionY = 0;
+                    }
+                    else
+                    {
+                        Person.CurentPositionY++;
+                    }
+                }
+                if (Person.Direction==2)//Vänster
+                {
+                    //if ()
+                    {
+
+                    }
+                }
+
+            }
+            //if (C.CurentPositionY == i)
+            //    C.CurentPositionY = 0;
+            //else
+            //    C.CurentPositionY++;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TjuvOchPolis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person.FillCityWithPeople();
+            GameBoard();
+            Console.ReadLine();
+
+        }
+
+        private static void GameBoard()
+        {
+            List<Person> Personlist = Person.PersonList;
+            int x = 50;
+            int y = 20;
+            string[,] gameboard = new string[x, y];
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    //string c="";
+                    foreach (var item in Personlist)
+                    {
+                        if (item.CurentPositionX == j && item.CurentPositionY == i)
+                        {
+                            Console.Write("M");
+
+                            //c = "M";
+                            //break;
+                        }
+                        else
+                        {
+                            //c = "-";
+                            Console.Write("-");
+                            break;
+                        }
+                    }
+                   // Console.Write(c);
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+            Console.Clear();
+
+           
+        }
+        
+    }
+}
